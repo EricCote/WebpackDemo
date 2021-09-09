f4859344c5fdb914d8ce06118f20feec6947f41f
Eric Cote <eric@coteexpert.com>
2021-09-09 14:06:03 -0400

    adding typescript

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import "bootstrap/dist/css/bootstrap.css"
+import createSubscription from "./subscribe"
+
+const btn = document.querySelector('#subscribe');
+btn.addEventListener('click', createSubscription);
